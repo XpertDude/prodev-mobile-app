36671ae59fb3d130dcc6ac5969c1ef3684b3a028
XpertDude <braodby@gmail.com>
2025-09-04 11:27:32 +0100

    Fix vommit

--- prodev-mobile-app-0x04/app/signin.tsx
+++ prodev-mobile-app-0x04/app/signin.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
-import { styles } from "../styles/_joinstyle"; // Import SignIn/Join shared styles
+import { styles } from "../styles/_join"; // Import SignIn/Join shared styles
 import { useNavigation } from "@react-navigation/native";
 
 const SignIn = () => {
